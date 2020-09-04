ae07459821cbbdca94575b77e4a21e2ee7e7fa72
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-09-04 00:40:33 -0400

    update for mithril 2

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -8,32 +8,32 @@ const {
 app.initializers.add('askvortsov/auth-sync', () => {
     app.extensionSettings['askvortsov-auth-sync'] = () =>
         app.modal.show(
-            new SettingsModal({
-                title: app.translator.trans('askvortsov-auth-sync.admin.title'),
-                type: 'small',
-                items: [
-                    <BooleanItem key="askvortsov-auth-sync.sync_avatar" required>
-                        {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_avatar')}
-                    </BooleanItem>,
-                    <StringItem key="askvortsov-auth-sync.ignored_avatar">
-                        {app.translator.trans('askvortsov-auth-sync.admin.labels.ignored_avatar')}
-                    </StringItem>,
-                    <BooleanItem key="askvortsov-auth-sync.stop_avatar_change" required>
-                        {app.translator.trans('askvortsov-auth-sync.admin.labels.stop_avatar_change')}
-                    </BooleanItem>,
-                    <BooleanItem key="askvortsov-auth-sync.sync_groups" required>
-                        {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_groups')}
-                    </BooleanItem>,
-                    <BooleanItem key="askvortsov-auth-sync.sync_bio" required>
-                        {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_bio')}
-                    </BooleanItem>,
-                    <BooleanItem key="askvortsov-auth-sync.stop_bio_change" required>
-                        {app.translator.trans('askvortsov-auth-sync.admin.labels.stop_bio_change')}
-                    </BooleanItem>,
-                    <BooleanItem key="askvortsov-auth-sync.sync_masquerade" required>
-                        {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_masquerade')}
-                    </BooleanItem>,
-                ],
-            })
+            SettingsModal, {
+            title: app.translator.trans('askvortsov-auth-sync.admin.title'),
+            type: 'small',
+            items: (s) => [
+                <BooleanItem name="askvortsov-auth-sync.sync_avatar" setting={s} required>
+                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_avatar')}
+                </BooleanItem>,
+                <StringItem name="askvortsov-auth-sync.ignored_avatar" setting={s}>
+                    {app.translator.trans('askvortsov-auth-sync.admin.labels.ignored_avatar')}
+                </StringItem>,
+                <BooleanItem name="askvortsov-auth-sync.stop_avatar_change" setting={s} required>
+                    {app.translator.trans('askvortsov-auth-sync.admin.labels.stop_avatar_change')}
+                </BooleanItem>,
+                <BooleanItem name="askvortsov-auth-sync.sync_groups" setting={s} required>
+                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_groups')}
+                </BooleanItem>,
+                <BooleanItem name="askvortsov-auth-sync.sync_bio" setting={s} required>
+                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_bio')}
+                </BooleanItem>,
+                <BooleanItem name="askvortsov-auth-sync.stop_bio_change" setting={s} required>
+                    {app.translator.trans('askvortsov-auth-sync.admin.labels.stop_bio_change')}
+                </BooleanItem>,
+                <BooleanItem name="askvortsov-auth-sync.sync_masquerade" setting={s} required>
+                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_masquerade')}
+                </BooleanItem>,
+            ],
+        }
         );
 });
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -4,14 +4,15 @@ import UserCard from 'flarum/components/UserCard';
 
 app.initializers.add('askvortsov/auth-sync', () => {
     override(UserCard.prototype, 'view', function (original) {
-        this.props.editable = false;
+        if (app.forum.attribute('stopAvatarChange')) {
+            this.attrs.editable = false;
+        }
 
         return original();
     });
 
 
-    extend(UserCard.prototype, 'config', function (isInitialized) {
-        if (isInitialized) return;
+    extend(UserCard.prototype, 'oncreate', function () {
         if (app.forum.attribute('stopBioChange') && $('.UserBio').hasClass('editable')) {
             if ($('.UserBio-content').find('.UserBio-placeholder').length !== 0) {
                 var styleTag = $('<style>.item-bio { display: none !important; }</style>')
