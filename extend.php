56fdcb1fe634e200334c3cc88e55f971f97180be
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-12-15 19:01:42 -0500

    Update for beta 15

--- extend.php
+++ extend.php
@@ -12,21 +12,25 @@
 namespace Askvortsov\FlarumAuthSync;
 
 use Flarum\Extend;
-use FoF\Components\Extend\AddFofComponents;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
-    new AddFofComponents(),
-
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
 
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js'),
 
+    (new Extend\Settings())
+        ->serializeToForum('stopAvatarChange', 'askvortsov-auth-sync.stop_avatar_change', function ($var) {
+            return (bool) $var;
+        })
+        ->serializeToForum('stopBioChange', 'askvortsov-auth-sync.stop_bio_change', function ($var) {
+            return (bool) $var;
+        }),
+
     function (Dispatcher $events) {
         $events->subscribe(Listener\UserUpdatedListener::class);
-        $events->subscribe(Listener\AddSettings::class);
     },
 
     new Extend\Locales(__DIR__.'/resources/locale'),
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,39 +1,39 @@
-import { settings } from '@fof-components';
-
-const {
-    SettingsModal,
-    items: { BooleanItem, StringItem },
-} = settings;
-
 app.initializers.add('askvortsov/auth-sync', () => {
-    app.extensionSettings['askvortsov-auth-sync'] = () =>
-        app.modal.show(
-            SettingsModal, {
-            title: app.translator.trans('askvortsov-auth-sync.admin.title'),
-            type: 'small',
-            items: (s) => [
-                <BooleanItem name="askvortsov-auth-sync.sync_avatar" setting={s} required>
-                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_avatar')}
-                </BooleanItem>,
-                <StringItem name="askvortsov-auth-sync.ignored_avatar" setting={s}>
-                    {app.translator.trans('askvortsov-auth-sync.admin.labels.ignored_avatar')}
-                </StringItem>,
-                <BooleanItem name="askvortsov-auth-sync.stop_avatar_change" setting={s} required>
-                    {app.translator.trans('askvortsov-auth-sync.admin.labels.stop_avatar_change')}
-                </BooleanItem>,
-                <BooleanItem name="askvortsov-auth-sync.sync_groups" setting={s} required>
-                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_groups')}
-                </BooleanItem>,
-                <BooleanItem name="askvortsov-auth-sync.sync_bio" setting={s} required>
-                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_bio')}
-                </BooleanItem>,
-                <BooleanItem name="askvortsov-auth-sync.stop_bio_change" setting={s} required>
-                    {app.translator.trans('askvortsov-auth-sync.admin.labels.stop_bio_change')}
-                </BooleanItem>,
-                <BooleanItem name="askvortsov-auth-sync.sync_masquerade" setting={s} required>
-                    {app.translator.trans('askvortsov-auth-sync.admin.labels.sync_masquerade')}
-                </BooleanItem>,
-            ],
-        }
-        );
+    app.extensionData
+        .for('askvortsov-auth-sync')
+        .registerSetting({
+            setting: 'askvortsov-auth-sync.sync_avatar',
+            label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_avatar'),
+            type: 'boolean'
+        })
+        .registerSetting({
+            setting: 'askvortsov-auth-sync.ignored_avatar',
+            label: app.translator.trans('askvortsov-auth-sync.admin.labels.ignored_avatar'),
+            type: 'text'
+        })
+        .registerSetting({
+            setting: 'askvortsov-auth-sync.stop_avatar_change',
+            label: app.translator.trans('askvortsov-auth-sync.admin.labels.stop_avatar_change'),
+            type: 'boolean'
+        })
+        .registerSetting({
+            setting: 'askvortsov-auth-sync.sync_groups',
+            label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_groups'),
+            type: 'boolean'
+        })
+        .registerSetting({
+            setting: 'askvortsov-auth-sync.sync_bio',
+            label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_bio'),
+            type: 'boolean'
+        })
+        .registerSetting({
+            setting: 'askvortsov-auth-sync.stop_bio_change',
+            label: app.translator.trans('askvortsov-auth-sync.admin.labels.stop_bio_change'),
+            type: 'boolean'
+        })
+        .registerSetting({
+            setting: 'askvortsov-auth-sync.sync_masquerade',
+            label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_masquerade'),
+            type: 'boolean'
+        })
 });
